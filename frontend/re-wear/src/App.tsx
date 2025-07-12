0e969c16d1d566fc7913a597a115e504850b64f0
manik prakash <manikprakash74@gmail.com>
2025-07-12 13:39:50 +0530

    init frontend nd backend

---
+++ frontend/re-wear/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex-1 items-center justify-center bg-amber-800'>
+        hiiiii
+      </div>
+    </>
+  )
+}
+
+export default App
